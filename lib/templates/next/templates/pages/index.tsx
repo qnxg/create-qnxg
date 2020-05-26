8ab5d2aac58801eed21e7ee168e2d7cc0abbcfc6
derekjqwang <derekjqwang@tencent.com>
2020-05-26 15:59:09 +0800

    feat: 增加next模板

---
+++ lib/templates/next/templates/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Index: React.FC = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>hello world</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default Index;
